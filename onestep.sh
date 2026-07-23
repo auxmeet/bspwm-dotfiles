c6071a6505461f686666a09f4608fb63e7ab79b0
auxmeet <mit35826@gmail.com>
2026-07-24 08:02:47 +0900

    Update onestep.sh

--- onestep.sh
+++ onestep.sh
@@ -41,35 +41,3 @@ for package in "${PACKAGESPACMAN[@]}"; do
         echo -e "${RED}✗ Ошибка при установке $package${NC}"
     fi
 done
-
-PACKAGESPARU=(
-    "picom-ftlabs-git"
-)
-
-# Обновление пакетов AUR
-echo -e "${YELLOW}Обновление paru...${NC}"
-paru -Syu --noconfirm
-
-# Установка утилит из AUR (выполняется БЕЗ sudo)
-echo -e "${YELLOW}Установка утилиты picom-ftlabs-git и обоев${NC}"
-for package in "${PACKAGESPARU[@]}"; do
-    echo "Установка $package..."
-    paru -S "$package" --noconfirm
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}✓ $package установлен${NC}"
-    else
-        echo -e "${RED}✗ Ошибка при установке $package${NC}"
-    fi
-done
-
-# Копирование обоев в пространство пользователя
-echo -e "${YELLOW}Копирование обоев..${NC}"
-mkdir -p "$HOME/wallpapers/"
-cp wall.jpg "$HOME/wallpapers/"
-
-# Копирование dotfiles БЕЗ использования sudo
-echo -e "${YELLOW}Копирование конфигов...${NC}"
-mkdir -p "$HOME/.config/"
-cp -r bspwm dunst kitty picom polybar rofi sxhkd "$HOME/.config/"
-
-echo -e "${GREEN}✓ Все готово!${NC}"
