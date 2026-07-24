98a544b9f3ceb9d7691d241d30da72d1973edeef
auxmeet <mit35826@gmail.com>
2026-07-25 07:16:37 +0900

    Update twostep.sh

--- twostep.sh
+++ twostep.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 NC='\033[0m'
-
 echo -e "${YELLOW}Установка picom и обоев...${NC}"
-
 PACKAGESPARU=(
     "picom-ftlabs-git"
 )
-
 echo -e "${YELLOW}Обновление paru...${NC}"
 paru -Syu --noconfirm
-
 echo -e "${YELLOW}Установка утилиты picom-ftlabs-git и обоев${NC}"
 for package in "${PACKAGESPARU[@]}"; do
     echo "Установка $package..."
@@ -24,11 +19,9 @@ for package in "${PACKAGESPARU[@]}"; do
         echo -e "${RED}✗ Ошибка при установке $package${NC}"
     fi
 done
-
 echo -e "${YELLOW}Копирование обоев..${NC}"
 mkdir -p "$HOME/wallpapers/"
 cp wall.jpg "$HOME/wallpapers/"
-
 echo -e "${YELLOW}Копирование конфигов...${NC}"
 mkdir -p "$HOME/.config/"
 sudo cp -r -v bspwm dunst kitty picom polybar rofi sxhkd "$HOME/.config/"
