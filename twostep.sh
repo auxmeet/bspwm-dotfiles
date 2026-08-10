28a93b23d2505be7534bf6a13309aa8de3e6f3e0
auxmeet <mit35826@gmail.com>
2026-08-10 23:22:57 +0900

    Update twostep.sh

--- twostep.sh
+++ twostep.sh
@@ -3,10 +3,20 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 NC='\033[0m'
-echo -e "${YELLOW}Installing paru...${NC}"
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
+echo -e "Istall from git or pacman"
+read -p "Continue? (pacman/git): " ans
+if [[ "$ans" == "git" ]]; then
+    echo -e "${YELLOW}Script will install paru from git${NC}"
+    echo -e "${YELLOW}Installing paru...${NC}"
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+fi
+if [[ "$ans" == "pacman" ]]; then
+    echo -e "${YELLOW}Script will install paru from pacman${NC}"
+    echo -e "${YELLOW}Installing paru...${NC}" 
+    sudo pacman -S paru --noconfirm --needed > /dev/null 2>&1
+fi
 echo -e "${YELLOW}Install picom-ftlabs-git and set wallpaper...${NC}"
 PACKAGESPARU=(
     "picom-ftlabs-git"
