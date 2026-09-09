f2e702aff45089cf9a91d6fe2c40e00329010399
auxmeet <mit35826@gmail.com>
2026-09-09 15:29:58 +0300

    Update and rename cachyos-config.fish to config.fish

--- cachyos-fish-config/config.fish
+++ cachyos-fish-config/config.fish
@@ -1,10 +1,10 @@
 ## Source from conf.d before our fish config
-source /usr/share/cachyos-fish-config/conf.d/done.fish
+source /usr/share/fish/done.fish
 
 ## Set values
 ## Run fastfetch as welcome message
 function fish_greeting
-    fastfetch --pipe --config ~/.config/fastfetch/fastfetch.jsonc
+    fastfetch --config ~/.config/fastfetch/fastfetch.jsonc
 end
 
 # Format man pages
@@ -74,17 +74,13 @@ function copy
     end
 end
 
-## Useful aliases
-# Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons=always' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons=always'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons=always'  # long format
-alias lt='eza -aT --color=always --group-directories-first --icons=always' # tree listing
-alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+alias ls='eza -al --color=always --group-directories-first --icons=always'
+alias la='eza -a --color=always --group-directories-first --icons=always'
+alias ll='eza -l --color=always --group-directories-first --icons=always'
+alias lt='eza -aT --color=always --group-directories-first --icons=always'
+alias l.="eza -a | grep -e '^\.'"
 
-# Common use
 alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-alias fixpacman="sudo rm /var/lib/pacman/db.lck"
 alias tarnow='tar -acf '
 alias untar='tar -zxvf '
 alias wget='wget -c '
@@ -100,19 +96,8 @@ alias vdir='vdir --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias hw='hwinfo --short'                                   # Hardware Info
-alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
-alias update='sudo cachyos-rate-mirrors && sudo pacman -Syu'
+alias hw='hwinfo --short'
+alias cleanup='sudo xbps-remove -o && sudo xbps-remove -y -O'
 
-# Get fastest mirrors
-alias mirror="sudo cachyos-rate-mirrors"
+alias clhis='history --clear'
 
-# Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-
-# Get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
-
-# Recent installed packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
